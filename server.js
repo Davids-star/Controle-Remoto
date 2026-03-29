ad113ec63f3af1cbe5728ce89a037a7e949aa927
Davids-star <davidsfelipe10@gmail.com>
2026-03-29 17:25:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-const { keyboard, Key } = require("@nut-tree-fork/nut-js")
+const { keyboard, Key, clipboard } = require("@nut-tree-fork/nut-js")
 const { mouse, Point, Button } = require("@nut-tree-fork/nut-js");
-const clipboardy = require("clipboardy")
 const { exec } = require('child_process')
 
 
@@ -14,14 +13,14 @@ app.use(cors())
 app.use(express.json())
 
 // Filtro de IP ativado globalmente (Antes até do HTML carregar)
-function ipFilter(req, res, next){
-  const ip = req.ip ? req.ip.replace("::ffff:", "") : "";
+function ipFilter(req, res, next) {
+    const ip = req.ip ? req.ip.replace("::ffff:", "") : "";
 
-  if(!ip.startsWith("192.168.18.48") && ip !== "127.0.0.1" && ip !== "::1"){
-    console.log("🚫 IP ACESSO NEGADO:", ip);
-    return res.status(403).send("Acesso totalmente bloqueado para este IP.");
-  }
-  next();
+    if (!ip.startsWith("192.168.18.48") && ip !== "127.0.0.1" && ip !== "::1") {
+        console.log("🚫 IP ACESSO NEGADO:", ip);
+        return res.status(403).send("Acesso totalmente bloqueado para este IP.");
+    }
+    next();
 }
 app.use(ipFilter);
 
@@ -30,12 +29,12 @@ app.use(express.static('site'))
 // Middleware de proteção por Token (Protege tudo que vier depois)
 app.use((req, res, next) => {
     const token = req.headers['x-token'];
-    
+
     if (token !== '3598') {
         console.log("⛔ API NEGADA. Token inválido! Recebido:", token);
         return res.status(403).json({ erro: 'Acesso negado: Token inválido' });
     }
-    
+
     let ip = req.ip ? req.ip.replace("::ffff:", "") : "desconhecido";
     console.log("✅ Comando autorizado. IP conectado:", ip);
     next();
@@ -45,7 +44,7 @@ app.post('/digitar', async (req, res) => {
     const { texto } = req.body
     console.log("Recebido do celular:", texto);
     // copia o texto pro clipboard
-    await clipboardy.write(texto);
+    await clipboard.setContent(texto);
 
     // cola o texto com Ctrl+V
     await keyboard.pressKey(Key.LeftControl, Key.V);
@@ -76,6 +75,13 @@ app.post('/seta', async (req, res) => {
     res.json({ ok: true });
 });
 
+//tela cheia 
+app.post('/tela_cheia', async (req, res) => {
+    console.log("Limpando corretamente...")
+    await keyboard.pressKey(Key.F11)
+    await keyboard.releaseKey(Key.F11)
+    res.json({ ok: true })
+})
 
 
 //apagar tudo (Ctrl+A + Delete)
@@ -83,7 +89,7 @@ app.post('/limpar', async (req, res) => {
     console.log("Limpando corretamente...")
 
     // coloca vazio no clipboard
-    await clipboardy.write("")
+    await clipboard.setContent("")
 
     // CTRL + A (seleciona tudo)
     await keyboard.pressKey(Key.LeftControl)
@@ -106,7 +112,7 @@ app.post('/limpar', async (req, res) => {
 })
 // desfazer crtl +z
 app.post('/desfazer', async (req, res) => {
-    await clipboardy.write("")
+    await clipboard.setContent("")
 
     await keyboard.pressKey(Key.LeftControl)
     await keyboard.pressKey(Key.Z)
@@ -120,7 +126,7 @@ app.post('/desfazer', async (req, res) => {
 })
 //crtl+y
 app.post('/refazer', async (req, res) => {
-    await clipboardy.write("")
+    await clipboard.setContent("")
 
     await keyboard.pressKey(Key.LeftControl)
     await keyboard.pressKey(Key.Y)
@@ -146,7 +152,7 @@ app.post('/linha', async (req, res) => {
 
 //Bloquear o pc
 app.post('/Bloquear_pc', async (req, res) => {
-    
+
     exec('rundll32.exe user32.dll,LockWorkStation');
     console.log("Bloqueando..")
     await new Promise(r => setTimeout(r, 150))
@@ -154,51 +160,56 @@ app.post('/Bloquear_pc', async (req, res) => {
     res.json({ ok: true })
 
 })
+//desligar o pc
+app.post('/desligar_pc', async(req, res)=>{ 
+    exec('msg * "O computador será desligado em 10 segundos"')
+    await new Promise(r => setTimeout(r,150 ))
+    exec("shutdown /s /t 10")
 
-//trocar janela
+    res.json({ ok : true})
+
+})
+//cancelar o deligamento do pc
+app.post('/cancelar_desligar', async(req, res) =>{
+
+    exec("shutdown /a")
+    await new Promise(r => setTimeout(r,150 ))
+
+    res.json({ ok : true})
+
+})
+
+// Trocar janela (Alt + Tab)
 app.post('/trocar_janela', async (req, res) => {
-    await clipboardy.write("")
-
     await keyboard.pressKey(Key.LeftAlt)
     await keyboard.pressKey(Key.Tab)
     await keyboard.releaseKey(Key.Tab)
     await keyboard.releaseKey(Key.LeftAlt)
 
-    console.log("trocando..")
-    await new Promise(r => setTimeout(r, 150))
-
+    console.log("Alt + Tab acionado")
     res.json({ ok: true })
-
 })
-//fechar pagina
-app.post('/fechar_janela', async (req, res) => {
-    await clipboardy.write("")
 
+// Fechar Guia / Aba (Ctrl + W ou Ctrl + F4)
+app.post('/fechar_guia', async (req, res) => {
     await keyboard.pressKey(Key.LeftControl)
-    await keyboard.pressKey(Key.F4)
-    await keyboard.releaseKey(Key.F4)
+    await keyboard.pressKey(Key.W)
+    await keyboard.releaseKey(Key.W)
     await keyboard.releaseKey(Key.LeftControl)
 
-    console.log("Bloqueando..")
-    await new Promise(r => setTimeout(r, 150))
-
+    console.log("Fechar guia acionado (Ctrl+W)")
     res.json({ ok: true })
-
 })
-//Fechar tudo
-app.post('/fechar_tudo', async (req, res) => {
-    await clipboardy.write("")
 
+// Fechar Tudo / Programa (Alt + F4)
+app.post('/fechar_tudo', async (req, res) => {
     await keyboard.pressKey(Key.LeftAlt)
     await keyboard.pressKey(Key.F4)
     await keyboard.releaseKey(Key.F4)
     await keyboard.releaseKey(Key.LeftAlt)
 
-    console.log("Bloqueando..")
-    await new Promise(r => setTimeout(r, 150))
-
+    console.log("Fechar programa acionado (Alt+F4)")
     res.json({ ok: true })
-
 })
 
 
@@ -245,29 +256,29 @@ app.post("/atualizar", async (req, res) => {
 });
 
 // Teclado numérico
-app.post('/numero', async (req,res) => {
-  const { n } = req.body;
+app.post('/numero', async (req, res) => {
+    const { n } = req.body;
 
-  if (n !== undefined) {
-    await keyboard.type(String(n));
-  }
+    if (n !== undefined) {
+        await keyboard.type(String(n));
+    }
 
-  res.json({ ok:true });
+    res.json({ ok: true });
 });
 
 //volume
 
 app.post('/volume-up', async (req, res) => {
-  await keyboard.pressKey(Key.AudioVolUp);
-  await keyboard.releaseKey(Key.AudioVolUp);
-  res.json({ ok: true });
+    await keyboard.pressKey(Key.AudioVolUp);
+    await keyboard.releaseKey(Key.AudioVolUp);
+    res.json({ ok: true });
 });
 
 // diminuir volume
 app.post('/volume-down', async (req, res) => {
-  await keyboard.pressKey(Key.AudioVolDown);
-  await keyboard.releaseKey(Key.AudioVolDown);
-  res.json({ ok: true });
+    await keyboard.pressKey(Key.AudioVolDown);
+    await keyboard.releaseKey(Key.AudioVolDown);
+    res.json({ ok: true });
 });
 
 
@@ -284,29 +295,19 @@ app.post('/mouse-mover', async (req, res) => {
     }
 })
 // position relative
-let posAtual = { x: 500, y: 300 }; // inicial
-mouse.move(new Point(posAtual.x, posAtual.y));
-
 app.post('/mouse-mover-relativo', async (req, res) => {
-    const { dx, dy } = req.body;
-    posAtual.x += dx*3;
-    posAtual.y += dy*3;
+    try {
+        const { dx, dy } = req.body;
+        const pos = await mouse.getPosition();
+        let newX = pos.x + (dx * 3);
+        let newY = pos.y + (dy * 3);
 
-    // 🔥 LIMITE TOTAL (AJUSTE AQUI)
-    const MIN_X = -1920; // monitor da esquerda
-    const MAX_X = 1920;  // monitor da direita
-    const MIN_Y = 0;
-    const MAX_Y = 1080;
-
-    // aplica limites
-    if (posAtual.x < MIN_X) posAtual.x = MIN_X;
-    if (posAtual.x > MAX_X) posAtual.x = MAX_X;
-
-    if (posAtual.y < MIN_Y) posAtual.y = MIN_Y;
-    if (posAtual.y > MAX_Y) posAtual.y = MAX_Y;
-    await mouse.move(new Point(posAtual.x, posAtual.y));
-
-    res.json({ ok: true });
+        await mouse.move(new Point(newX, newY));
+        res.json({ ok: true });
+    } catch (err) {
+        console.error("Erro ao mover mouse:", err);
+        res.status(500).json({ ok: false });
+    }
 });
 
 // Clique esquerdo
@@ -318,6 +319,18 @@ app.post('/mouse-clique', async (req, res) => {
 // Clique direito
 app.post('/mouse-clique-direito', async (req, res) => {
     await mouse.rightClick();
+    res.json({ ok: true });
+});
+
+// Pressionar botão esquerdo (para arrastar)
+app.post('/mouse-down', async (req, res) => {
+    await mouse.pressButton(Button.LEFT);
+    res.json({ ok: true });
+});
+
+// Soltar botão esquerdo
+app.post('/mouse-up', async (req, res) => {
+    await mouse.releaseButton(Button.LEFT);
     res.json({ ok: true });
 });
 
